74f3bc30c995abd53a55ee6afdbe69aa60e854bf
mistaru@bk.ru <38661036+mistaru@users.noreply.github.com>
2024-07-17 20:28:21 +0600

    сделал изменения

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Мы сегодня изучаем версионность - Git");
         System.out.println("Изменил новую строку для демонстрации");
+        System.out.println("Изменил новую строку для демонстрации");
+        System.out.println("Изменил новую строку для демонстрации");
+        System.out.println("Изменил новую строку для демонстрации");
+        System.out.println("Изменил новую строку для демонстрации");
+        System.out.println("Изменил новую строку для демонстрации");
+        System.out.println("Изменил новую строку для демонстрации");
+        System.out.println("Изменил новую строку для демонстрации");
+        System.out.println("Изменил новую строку для демонстрации");
     }
 }
